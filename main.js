a6779a3663b5d5f4b7669efcff81b5c6fcb702a4
lexria <alexandriamlewis@me.com>
2016-03-02 17:56:00 -0500

    cleaned up js and added comments

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
     page.init();
 });
 var page = {
+    //storage for the search function
     dataStore: [],
+    //storage for the favorites function
     favoriteStore: [],
-    url: "http://api.nytimes.com/svc/topstories/v1/", //section
+    url: "http://api.nytimes.com/svc/topstories/v1/",
     section: "home",
     key: ".json?api-key=0c061decbcee9fc4a2a618b408849de6:18:74588993",
     init: function() {
@@ -12,6 +14,7 @@ var page = {
         page.initEvents();
     },
     initStyling: function() {
+        //getting the home section loaded
         page.getData();
     },
     initEvents: function() {
@@ -20,14 +23,14 @@ var page = {
             event.preventDefault();
             var section = $(this).text();
             var link = page.clickedSection(section);
-            // console.log(link);
+            //on click build page
             page.linkedPage(link);
         });
-        // click on Search Bar
+        // submit into Search Bar
         $('form').on('submit', function(event) {
             event.preventDefault();
             var userSubmit = $('input[name="search"]').val();
-            // console.log("USER SUBMIT",userSubmit);
+            //on submit build page
             page.searchResults(userSubmit);
         });
         // change css when search bar clicked
@@ -37,6 +40,7 @@ var page = {
                 'color': '#fff'
             });
         });
+        //clicking on favorite hearts <3
         $('.mainContainer').on('click', '.fa-heart-o', function(event) {
                 event.preventDefault();
                 console.log('I WAS CLICKED');
@@ -72,22 +76,27 @@ var page = {
             addAllLikes(completed);
         });
     },
+    //to build new url to load by section
     clickedSection: function(section) {
         return page.url + section + page.key;
-        // console.log(page.url + section + page.key);
     },
+    //work with submitted search keyword/s
     searchResults: function(userSubmit) {
         //had to create a literal variable to search case insensitive
         var regex = new RegExp(userSubmit, 'i');
-        console.log(regex);
+        //mapping over entire object
         var matchedObj = _.map(page.dataStore, function(object) {
+          // filtering for matches only
             var filteredObjs = _.filter(object, function(el) {
                 return el.title.match(regex) || el.blurb
                     .match(regex)
             });
+            //putting filtered objects into new mapped array/object
             return filteredObjs;
         });
+        //had to clear container before appending new content
         $('div.mainContainer').html('');
+        //loop through each object and append to page
         _.each(matchedObj[0], function(el) {
             var tmpl = _.template(templates.post);
             $('div.mainContainer').append(tmpl(el));
@@ -95,16 +104,18 @@ var page = {
                 .scrollHeight);
         })
     },
+    //building intitial url for page load
     buildUrl: function() {
         return page.url + page.section + page.key;
     },
+    //getter ajax for initial styling
     getData: function() {
         $.ajax({
             method: 'GET',
             url: page.buildUrl(),
             dataType: 'json',
             success: function(data) {
-                window.glob = data;
+                // window.glob = data;
                 page.addDataToPage(data);
             },
             error: function(err) {
@@ -112,6 +123,7 @@ var page = {
             }
         })
     },
+    //to build the page on nav click get the content
     linkedPage: function(link) {
         $.ajax({
             method: 'GET',
@@ -119,9 +131,7 @@ var page = {
             dataType: 'json',
             success: function(data) {
                 window.glob = data;
-                // page.getDataObj(data);
                 page.addDataToPage(data);
-                // console.log(newArr);
             },
             error: function(err) {
                 console.log('error!', err)
@@ -130,9 +140,11 @@ var page = {
             }
         })
     },
+    //creating the object with the right keys for template used in addDataToPage
     getDataObj: function(data) {
         return _.map(data.results, function(el) {
             var imgUrl = "";
+            //needed to filter for just articles with pictures
             if (el.multimedia[3]) {
                 imgUrl = el.multimedia[3].url;
             }
@@ -147,6 +159,7 @@ var page = {
             }
         })
     },
+    //to apply template and append to page
     addDataToPage: function(dataObj) {
         var newArr = page.getDataObj(dataObj);
         page.favoriteStore = [];
@@ -155,7 +168,6 @@ var page = {
             return el.image
         })
         page.dataStore.splice(0, 1, filteredArr);
-        // console.log(page.dataStore);
         $('div.mainContainer').html('');
         _.each(filteredArr, function(el) {
             var tmpl = _.template(templates.post);
