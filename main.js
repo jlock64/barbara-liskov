6efb05453f92483b18a98aa4ae33a0e119130e53
lexria <alexandriamlewis@me.com>
2016-03-02 11:40:14 -0500

    performed dirtymarkup on html and js

--- main.js
+++ main.js
@@ -1,135 +1,107 @@
-$(document).ready(function(){
-  page.init();
-
+$(document).ready(function() {
+    page.init();
 });
-
-var page ={
-url: "http://api.nytimes.com/svc/topstories/v1/", //section
-section: "home",
-key: ".json?api-key=0c061decbcee9fc4a2a618b408849de6:18:74588993",
-
-init: function(){
-  page.initStyling();
-  page.initEvents();
-},
-initStyling: function(){
-  page.getData();
-},
-initEvents: function(){
-  $('ul li').on('click', function(event){
-    event.preventDefault();
-    var section = $(this).text();
-    var link = page.clickedSection(section);
-    console.log(link);
-    page.newPage(link);
-  });
-
-  $('form').on('submit', function(event) {
-    event.preventDefault();
-    var userSubmit = $('input[name="search"]').val();
-    console.log(userSubmit);
-    page.searchResults(userSubmit);
-  })
-},
-
-  searchResults: function(userSubmit) {
-
-  },
-
-  getSearchObj: function(data){
-    return _.map(data.results, function(el){
-      var imgUrl = "";
-      if(el.multimedia[3]) {
-        imgUrl = el.multimedia[3].url;
-      }
-      return {
-        title: el.title,
-        blurb: el.abstract,
-        url: el.url,
-        subsection: el.subsection,
-        date: moment(el.published_date).format('LL'),
-        image: imgUrl
-      }
-    })
-  },
-
-buildUrl: function(){
-  return page.url + page.section + page.key;
-},
-
-getData: function () {
-  $.ajax({
-    method:'GET',
-    url: page.buildUrl(),
-    dataType: "json",
-    success: function(data){
-      window.glob = data;
-      page.addDataToPage(data);
-
+var page = {
+    url: "http://api.nytimes.com/svc/topstories/v1/", //section
+    section: "home",
+    key: ".json?api-key=0c061decbcee9fc4a2a618b408849de6:18:74588993",
+    init: function() {
+        page.initStyling();
+        page.initEvents();
     },
-    error: function(err){
-      console.log(err)
-    }
-  })
-
-},
-
-newPage: function (link) {
-  $.ajax({
-    method:'GET',
-    url: link,
-    dataType: "json",
-    success: function(data){
-      window.glob = data;
-      // page.getDataObj(data);
-      page.addDataToPage(data);
-      // console.log(newArr);
-
+    initStyling: function() {
+        page.getData();
     },
-    error: function(err){
-      console.log(err)
+    initEvents: function() {
+        $('ul li').on('click', function(event) {
+            event.preventDefault();
+            var section = $(this).text();
+            var link = page.clickedSection(section);
+            console.log(link);
+            page.newPage(link);
+        });
+        $('form').on('submit', function(event) {
+            event.preventDefault();
+            var userSubmit = $('input[name="search"]').val();
+            console.log(userSubmit);
+            page.searchResults(userSubmit);
+        })
+    },
+    searchResults: function(userSubmit) {},
+    getSearchObj: function(data) {
+        return _.map(data.results, function(el) {
+            var imgUrl = "";
+            if (el.multimedia[3]) {
+                imgUrl = el.multimedia[3].url;
+            }
+            return {
+                title: el.title,
+                blurb: el.abstract,
+                url: el.url,
+                subsection: el.subsection,
+                date: moment(el.published_date).format('LL'),
+                image: imgUrl
+            }
+        })
+    },
+    buildUrl: function() {
+        return page.url + page.section + page.key;
+    },
+    getData: function() {
+        $.ajax({
+            method: 'GET',
+            url: page.buildUrl(),
+            dataType: "json",
+            success: function(data) {
+                window.glob = data;
+                page.addDataToPage(data);
+            },
+            error: function(err) {
+                console.log(err)
+            }
+        })
+    },
+    newPage: function(link) {
+        $.ajax({
+            method: 'GET',
+            url: link,
+            dataType: "json",
+            success: function(data) {
+                window.glob = data;
+                // page.getDataObj(data);
+                page.addDataToPage(data);
+                // console.log(newArr);
+            },
+            error: function(err) {
+                console.log(err)
+            }
+        })
+    },
+    getDataObj: function(data) {
+        return _.map(data.results, function(el) {
+            var imgUrl = "";
+            if (el.multimedia[3]) {
+                imgUrl = el.multimedia[3].url;
+            }
+            return {
+                title: el.title,
+                blurb: el.abstract,
+                url: el.url,
+                subsection: el.subsection,
+                date: moment(el.published_date).format('LL'),
+                image: imgUrl
+            }
+        })
+    },
+    addDataToPage: function(dataObj) {
+        var newArr = page.getDataObj(dataObj);
+        var filteredArr = _.filter(newArr, function(el) {
+            return el.image
+        })
+        $('div.mainContainer').html('');
+        _.each(filteredArr, function(el) {
+            var tmpl = _.template(templates.post);
+            $('div.mainContainer').append(tmpl(el));
+        })
     }
-  })
-
-},
-
-
-getDataObj: function(data){
-  return _.map(data.results, function(el){
-    var imgUrl = "";
-    if(el.multimedia[3]) {
-      imgUrl = el.multimedia[3].url;
-    }
-    return {
-      title: el.title,
-      blurb: el.abstract,
-      url: el.url,
-      subsection: el.subsection,
-      date: moment(el.published_date).format('LL'),
-      image: imgUrl
-    }
-  })
-},
-
-
-addDataToPage: function(dataObj){
-  var newArr = page.getDataObj(dataObj);
-  var filteredArr = _.filter(newArr, function (el){
-    return el.image
-  })
-  $('div.mainContainer').html('');
-  _.each(filteredArr, function (el) {
-    var tmpl = _.template(templates.post);
-    $('div.mainContainer').append(tmpl(el));
-  })
-}
-
-
-
-
-
-
-
-
-
 }
